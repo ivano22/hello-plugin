98e35d6f72b394d04e7c50d4743395cd28dd4cc0
ivano22 <ituryomunsi@gmail.com>
2021-06-01 18:35:23 +0300

    Create helloplugin.php

---
+++ helloplugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hello plugin
+Plugin URI: http://techhub.com
+description: a plugin to say hello and spread joy
+Version: 1.5
+Author: Ivan
+Author URI: http://techhub.com
+
+
+*/
